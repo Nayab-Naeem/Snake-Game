3915ae135761d476c4b86fafb8ed01440a37fa46
Nayab Naeem <nayabnaeem666@gmail.com>
2026-01-09 19:30:49 +0500

    Code for the Structure of Snake

--- main.asm
+++ main.asm
@@ -96,3 +96,71 @@ RightWall:
 
     RET
 DrawWalls ENDP
+
+;--------------------------------------------
+;Function for making snake 
+
+DrawSnake PROC
+    MOV CL,0          ; segment counter
+    MOV SI,0          ; index for SnakeX/Y arrays
+
+DrawSnakeLoop:
+    CMP CL, SnakeLen
+    JGE DoneSnake
+
+    ; Move cursor to segment position
+    MOV AH,02h
+    MOV BH,0
+    MOV DH, SnakeY[SI] ; Row
+    MOV DL, SnakeX[SI] ; Column
+    INT 10h
+
+    ; Determine character to draw
+    CMP CL,0           ; Is this the head?
+    JE DrawHead
+    MOV AL,219         ; Body = full block █
+    MOV BL,0Bh         ; Body color = cyan
+    JMP PrintChar
+
+DrawHead:
+    ; Set head character based on direction of the movement
+    MOV AL,'^'         ; default Up
+    MOV BL,0Ch         ; Head color = red
+    MOV AL, SnakeDir
+    CMP AL,0           ; Up
+    JE HeadUp
+    CMP AL,1           ; Down
+    JE HeadDown
+    CMP AL,2           ; Left
+    JE HeadLeft
+    CMP AL,3           ; Right
+    JE HeadRight
+    JMP HeadUp        ; fallback
+
+HeadUp:
+    MOV AL,'^'
+    JMP PrintChar
+HeadDown:
+    MOV AL,'v'
+    JMP PrintChar
+HeadLeft:
+    MOV AL,'<'
+    JMP PrintChar
+HeadRight:
+    MOV AL,'>'
+    JMP PrintChar
+
+    ;Display the character
+PrintChar:
+    MOV AH,09h
+    MOV CX,1
+    INT 10h
+
+    ; Next segment
+    INC SI
+    INC CL
+    JMP DrawSnakeLoop
+
+DoneSnake:
+    RET
+DrawSnake ENDP
